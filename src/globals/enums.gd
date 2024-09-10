4b87140c03bf5d5dca4cba6c82c81d3aab7f32f5
Eeviika <84326942+Eeviika@users.noreply.github.com>
2024-09-09 22:53:21 -0400

    Added ErrorCodes enum (3 so far)

--- src/globals/enums.gd
+++ src/globals/enums.gd
@@ -1,5 +1,7 @@
 extends Object
 class_name GlobalEnums
 
-enum State {EGG, NEUTRAL, HAPPY, SICK, UPSET, RANAWAY}
+enum State {EGG, NEUTRAL, HAPPY, SICK, UPSET, RAN_AWAY}
 enum Difficulty {RELAXED, STANDARD, REALISTIC}
+# NONE is an ErrorCode because 0 = success.
+enum ErrorCodes {NONE, GENERIC, PREFORMANCE_FAILSAFE}
