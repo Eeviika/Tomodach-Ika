9424f738afb3f2b283817bf463394deba6aa2933
Eeviika <84326942+Eeviika@users.noreply.github.com>
2024-09-09 22:54:05 -0400

    Added a preformance failsafe to clock, so if the game starts running slowly especially after catching up to standard tick rate, we go to title screen (currently quit right now)

--- src/globals/clock.gd
+++ src/globals/clock.gd
@@ -2,9 +2,12 @@ extends Node
 
 signal Tick(CurrentTime: float)
 
-var CurrentTime: float
-var TickTime: float
 var TicksPassed: int = 0
+
+var DangerZone: bool = false
+
+var CurrentTime: float = 0.0
+var TickTime: float = 0.0
 var TickInterval: float = 0.05
 var Offset: float = 0.0:
     get:
@@ -24,8 +27,15 @@ func _process(delta: float) -> void:
     CurrentTime += delta
     if delta >= 1:
         print("[CLOCK / WARN]: Delta was 1 or higher.  Delta value was: ", delta)
-    if CurrentTime >= TickTime + (TickInterval*2.5):
-        print("[CLOCK / WARN]: Catching up to regular tick rate!")
+    if delta >= 3 and DangerZone:
+        print("[CLOCK / WARN]: Game exceeded 3 seconds of delta time and we attempted to catch up to tick rate last frame! Returning to title screen...")
+        # TODO: Player should return to title screen here. For now, we'll just close the game. Since games aren't being saved right now, I'll change this later.
+        get_tree().quit(GlobalEnums.ErrorCodes.PREFORMANCE_FAILSAFE)
+        return
+    if CurrentTime >= TickTime + (TickInterval*3):
+        print("[CLOCK / WARN]: Last tick took %fs, is the game being slowed down? (Ticks should typically take %fs - %fs for this game...)" % [delta, TickInterval*.8, TickInterval*1.2])
+        print("[CLOCK / WARN]: If the game freezes for up to 3 seconds past this, the game will be saved and will return to the title screen.")
+        DangerZone = true
     while CurrentTime >= TickTime + TickInterval:
         Tick.emit(CurrentTime)
         TickTime += TickInterval
