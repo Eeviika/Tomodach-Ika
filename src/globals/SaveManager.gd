a31f150d53708a330d1f08839673188e65638a11
Eeviika <84326942+Eeviika@users.noreply.github.com>
2024-09-10 19:12:14 -0400

    Organized the EmptySave a bit more, added comments, and added PetData with some additional pet attributes.

--- src/globals/SaveManager.gd
+++ src/globals/SaveManager.gd
@@ -10,14 +10,26 @@ const AutoLoadData: bool = false
 const SaveInDebug: bool = false
 
 const EmptySave: Dictionary = {
-	"CurrentPet": null,
-	"Evolution": null,
-	"Hunger": 0,
-	"Enjoyment": 0,
-	"Happiness": 0,
-	"EvolutionHappiness": 0,
-	"Difficulty": GlobalEnums.Difficulty.STANDARD,
-	"State": GlobalEnums.State.EGG,
+    "Name": "",                                     # Name of the player.
+	"CurrentPetNamespace": null,                    # Namespace of the pet's data file. If a mod named "Example" added a pet named "Pet", the namespace would be "Example/Pet"
+    "Difficulty": GlobalEnums.DIFFICULTY.STANDARD,  # The game difficulty.
+    "LastSave": 0,                                  # Unix timestamp of when the game was last saved.
+    "Birthday": 0,                                  # Unix timestamp of the player's birthday.
+    "PetData": {
+        "Name": "",                                 # Name of the pet. Used as a fallback.
+        "Nickname": "",                             # Player-generated nickname of the pet.
+        "OriginalOwner": "",                        # Name of the player.
+        "State": GlobalEnums.STATE.EGG,             # State of the pet.
+        "Gender": GlobalEnums.GENDER.GENDERLESS,    # Gender of the pet.
+        "AdoptionTime": 0,                          # Unix timestamp of when the pet was adopted.                            
+        "Weight": 0,                                # Weight of the pet. Is not AverageWeight.
+        "Height": 0,                                # Height of the pet. Is not AverageHeight.
+        "Hunger": 0,                                # How hungry the pet is from 1 - 100, where 100 is full, and 0 is starving.
+        "Enjoyment": 0,                             # How much the pet enjoys the player's company.
+        "Happiness": 0,                             # How happy the pet is with the player.
+        "EvolutionHappiness": 0,                    # How inclined the pet is to evolve.
+        "FlavorPreference": 0,                      # The pet's flavor preference.
+    }
 }
 
 var SaveSlot: int = 0
