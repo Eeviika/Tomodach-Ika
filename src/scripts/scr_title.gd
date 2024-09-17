fdf2e8173c36e7de8b8b120132da4ddd82ae748a
Eeviika <84326942+Eeviika@users.noreply.github.com>
2024-09-17 16:14:42 -0400

    ???

--- src/scripts/scr_title.gd
+++ src/scripts/scr_title.gd
@@ -3,6 +3,4 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if OS.is_debug_build():
-		OS.alert("", "DEBUG WARNING")
 	pass # Replace with function body.
