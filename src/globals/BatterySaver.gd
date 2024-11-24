677a6f04515f0ad81fa61f25fe520ed3cd01cc39
Eeviika <84326942+Eeviika@users.noreply.github.com>
2024-11-23 19:27:19 -0500

    Implement battery saver functionality with idle timer and FPS adjustments

--- src/globals/BatterySaver.gd
+++ src/globals/BatterySaver.gd
@@ -1,9 +1,10 @@
 extends Node
 
 const ENABLE_ON_DEBUG: bool = false
-const IDLE_TIME: int = 0
+const IDLE_TIME: int = 60
 
 var IdleTimer: Timer
+
 var logger: Logger
 var max_fps: float
 
@@ -17,11 +18,33 @@ func _ready() -> void:
 		queue_free()
 		return
 	
-	IdleTimer = Timer.new()
-	add_child(IdleTimer)
+	logger.info("Battery Saver is enabled.")
+
+	max_fps = ProjectSettings.get_setting("application/run/max_fps")
 	
+	IdleTimer = Timer.new()
 	IdleTimer.wait_time = IDLE_TIME
+	IdleTimer.one_shot = true
+	IdleTimer.autostart = true
+	add_child(IdleTimer)
 
+	IdleTimer.timeout.connect(_idle_timeout)
+	
+func _idle_timeout() -> void:
+	logger.info("Idle timeout reached. Setting max FPS to %f." %  int(max_fps / 2))
+	ProjectSettings.set_setting("application/run/max_fps", int(max_fps / 2))
 
-# func _process(delta: float) -> void:
-# 	pass
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion or event is InputEventScreenTouch or event is InputEventKey:
+		IdleTimer.start()
+		if ProjectSettings.get_setting("application/run/max_fps") != max_fps:
+			logger.info("User input detected. Setting max FPS to %f." % int(max_fps))
+			ProjectSettings.set_setting("application/run/max_fps", max_fps)
+
+func _process(_delta: float) -> void:
+	if not DisplayServer.window_is_focused() and not ProjectSettings.get_setting("application/run/low_processor_mode"):
+		logger.info("Window is not focused. Beginning low processor mode.")
+		ProjectSettings.set_setting("application/run/low_processor_mode", true)
+	elif DisplayServer.window_is_focused() and ProjectSettings.get_setting("application/run/low_processor_mode"):
+		logger.info("Window is focused. Ending low processor mode.")
+		ProjectSettings.set_setting("application/run/low_processor_mode", false)
