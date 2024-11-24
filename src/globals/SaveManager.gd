96ead3d0d00ec689cd0bc43ea84e7e88c0f0e3b0
Eeviika <84326942+Eeviika@users.noreply.github.com>
2024-11-23 19:07:47 -0500

    Refactor SaveManager to use Logger for improved logging

--- src/globals/SaveManager.gd
+++ src/globals/SaveManager.gd
@@ -45,67 +45,69 @@ const EmptySave: Dictionary = {
 
 var save_slot: int = 0
 var current_save: Dictionary
+var logger: Logger
 
 func update_data() -> void:
 	if not current_save:
-		print("[SAVE_MANAGER / WARN]: Attempted to update save data without loading it first!")
+		logger.warn("Attempted to update save data without loading it first!")
 		return
 	if current_save.has_all(EmptySave.keys()):
-		print("[SAVE_MANAGER / INFO]: No data to update.")
+		logger.info("No data to update.")
 		return
-	print("[SAVE_MANAGER / INFO]: Updating save data.")
+	logger.info("Updating save data.")
 	for key: String in EmptySave.keys():
 		current_save.get_or_add(key, EmptySave[key])
 
 ## Attempts to load data from disk. If data is already loaded and [code]<force: bool>[/code] is off then data will not be loaded from disk.
 func load_data(force: bool = false) -> void:
 	if current_save and not (force or current_save == EmptySave):
-		print("[SAVE_MANAGER / INFO]: Save already loaded, not loading again since param FORCE is off.")
+		logger.info("Save already loaded, not loading again since param FORCE is off.")
 		return
 	BeforeLoadedData.emit()
 	if not FileAccess.file_exists("user://d_slot%s.sav" % save_slot):
-		print("[SAVE_MANAGER / INFO]: Generating save data in memory since a save file in slot %s doesn't exist." % save_slot)
+		logger.info("Generating save data in memory since a save file in slot %s doesn't exist." % save_slot)
 		current_save = EmptySave
 		LoadedData.emit(current_save)
 		return
 	var SaveFile: FileAccess = FileAccess.open("user://d_slot%s.sav" % save_slot, FileAccess.READ)
 	if SaveFile == null:
-		print("[SAVE_MANAGER / WARN]: Failed to load save! See error below:")
-		print(FileAccess.get_open_error())
+		logger.warn("Failed to load save! See error below:")
+		logger.error(error_string(FileAccess.get_open_error()))
 		return
 	current_save = SaveFile.get_var()
 	update_data()
 	LoadedData.emit(current_save)
 	SaveFile.close()
-	print("[SAVE_MANAGER / INFO]: Loaded save file at slot %s." % save_slot)
+	logger.info("Loaded save file at slot %s." % save_slot)
 
 ## Saves the current data to disk.
 func save_data(data: Dictionary) -> void:
 	if not current_save:
-		print("[SAVE_MANAGER / WARN] No data to save! Not saving data.")
+		logger.warn("No data to save! Not saving data.")
 		return
 	if not current_save.started_game:
-		print("[SAVE_MANAGER / WARN: Didn't pass tutorial! Not saving data.")
+		logger.warn("Didn't pass tutorial! Not saving data.")
 		return
 	if OS.is_debug_build() and not SaveInDebug:
-		print("[SAVE_MANAGER / INFO]: SaveInDebug is false, so the current file will not be saved.")
+		logger.info("SaveInDebug is false, so the current file will not be saved.")
 		return
 	BeforeSavedData.emit()
 	SavedData.emit(data)
 	pass
 
 func _ready() -> void:
+	logger = Logger.new(self)
 	if AutoSave:
 		var save_timer: Timer = Timer.new()
 		save_timer.wait_time = AutoSaveInterval
 		save_timer.one_shot = false
 		save_timer.autostart = true
 		save_timer.timeout.connect(func()->void:
-			print("[SAVE_MANAGER / INFO] Attempting autosave.")
+			logger.info("Attempting autosave.")
 			save_data(current_save)
 		)
 		add_child(save_timer)
 	if AutoLoadData:
-		print("[SAVE_MANAGER / INFO]: Autoloading data.")
+		logger.info("Autoloading data.")
 		load_data()
 		pass
